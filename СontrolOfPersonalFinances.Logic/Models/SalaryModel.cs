faebccdb9b4a3b10f792e0238e3774bd51571c37
ustinova <rykova.tanya.96@gmail.com>
2023-12-26 15:13:01 +0300

    add

--- СontrolOfPersonalFinances.Logic/Models/SalaryModel.cs
+++ СontrolOfPersonalFinances.Logic/Models/SalaryModel.cs
@@ -14,5 +14,21 @@ namespace СontrolOfPersonalFinances.Logic.Models
         public decimal Summ {  get; set; }
         public DataSetDateTime dateTime { get; set; }
          
+        public void SendingASalary(AccountModel account,int ammount)
+        {
+            DateTime currentDate = DateTime.Now;
+            SalaryModel salary = new SalaryModel();
+
+            // Проверяем, является ли текущая дата 15 числом месяца
+            if (currentDate.Day == 15)
+            {
+                salary.NumberAccount= "Работодатель";
+                salary.Summ = ammount;
+            };
+            account.Balance += salary.Summ;
+            ShowAccountsList.Items.Add($"Баланс пополнен на {salary.Summ} рублей.");
+
+        }
     }
+
 }
--- СontrolOfPersonalFinances/MainWindow.xaml.cs
+++ СontrolOfPersonalFinances/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows;
 using System.Windows.Controls;
 using СontrolOfPersonalFinances.Logic.Enums;
 using СontrolOfPersonalFinances.Logic.Model;
+using СontrolOfPersonalFinances.Logic.Models;
 
 namespace СontrolOfPersonalFinances
 {
@@ -21,110 +22,69 @@ namespace СontrolOfPersonalFinances
         private void OkButton_Click(object sender, RoutedEventArgs e)
         {
             string currentTime = DateTime.Now.ToString("HH:mm:ss");
-            string selectedItem = "";
-
-            if (Account.IsChecked == true)
-            {
-                selectedItem = "Кредитная карта";
-                AccountModel newAccount = new AccountModel();
-                newAccount.AccountNumber = NumberLabelText.Text;
+           
+               AccountModel newAccount = new AccountModel();
+               newAccount.AccountNumber = NumberLabelText.Text;
                newAccount.BankName = TextBoxBank.Text;
-                newAccount.Balance = decimal.Parse(BalansLabelText.Text);
-                newAccount.Currency = TextBoxCurrent.Text;
-                newAccount.Comment = CommentsBox.Text;
-                newAccount.Id = IdBox.Text; 
+               newAccount.Balance = decimal.Parse(BalansLabelText.Text);
+               newAccount.Currency = TextBoxCurrent.Text;
+               newAccount.Comment = CommentsBox.Text;
+               newAccount.Id = Int32.Parse(IdBox.Text); 
+               _accountingSystem._accounts.Add(Convert.ToInt32(AccountType.Cash), newAccount);
+               ListBoxOne.Items.Add($"{currentTime} Номер счета: {newAccount.AccountNumber}, Название банка: {newAccount.BankName}, Баланс: {newAccount.Balance}, Валюта: {newAccount.Currency} , Ваш ID : {newAccount.Id}, Комментарий:{newAccount.Comment}");
+               ListBoxOne.Items.Clear();
 
-                _accountingSystem._accounts.Add(AccountType.CreditCard,newAccount);
-                ListBoxOne.Items.Add($"{currentTime}Тип: {selectedItem}, Номер счета: {newAccount.AccountNumber}, Название банка: {newAccount.BankName}, Баланс: {newAccount.Balance}, Валюта: {newAccount.Currency} ,ID операции {newAccount.Id},Комментарий:{newAccount.Comment}");
-            }
-            else if (Cash.IsChecked == true)
-            {
-                selectedItem = "Наличные";
-                AccountModel newAccount = new AccountModel();
-                newAccount.AccountNumber = NumberLabelText.Text;
-                newAccount.BankName = TextBoxBank.Text;
-                newAccount.Balance = decimal.Parse(BalansLabelText.Text);
-                newAccount.Currency =TextBoxCurrent.Text;
-                newAccount.Comment = CommentsBox.Text;
-                newAccount.Id = IdBox.Text;
-
-                _accountingSystem._accounts.Add(AccountType.Cash, newAccount);
-                ListBoxOne.Items.Add($"{currentTime}Тип: {selectedItem}, Номер счета: {newAccount.AccountNumber}, Название банка: {newAccount.BankName}, Баланс: {newAccount.Balance}, Валюта: {newAccount.Currency}");
-
-            }
-
-            else if (DebetCart.IsChecked == true)
-            {
-                selectedItem = "Дебетовая карта";
-                AccountModel newAccount = new AccountModel();
-                newAccount.AccountNumber = NumberLabelText.Text;
-                newAccount.BankName = TextBoxBank.Text;
-                newAccount.Balance = decimal.Parse(BalansLabelText.Text);
-                newAccount.Currency = TextBoxCurrent.Text;
-                newAccount.Comment = CommentsBox.Text;
-                newAccount.Id = IdBox.Text;
-
-                _accountingSystem._accounts.Add(AccountType.DebetCard, newAccount);
-                ListBoxOne.Items.Add($"{currentTime}Тип: {selectedItem}, Номер счета: {newAccount.AccountNumber}, Название банка: {newAccount.BankName}, Баланс: {newAccount.Balance}, Валюта: {newAccount.Currency}, ID операции {newAccount.Id},Комментарий:{newAccount.Comment}");
-            }
-            else if (Debt.IsChecked == true)
-            {
-                selectedItem = "Долг";
-                AccountModel newAccount = new AccountModel();
-                newAccount.AccountNumber = NumberLabelText.Text;
-                newAccount.BankName =TextBoxBank.Text;
-                newAccount.Balance = decimal.Parse(BalansLabelText.Text);
-                newAccount.Currency = TextBoxCurrent.Text;
-                newAccount.Comment = CommentsBox.Text;
-                newAccount.Id = IdBox.Text;
-
-                _accountingSystem._accounts.Add(AccountType.Debt, newAccount);
-                ListBoxOne.Items.Add($"{currentTime}Тип: {selectedItem}, Номер счета: {newAccount.AccountNumber}, Название банка: {newAccount.BankName}, Баланс: {newAccount.Balance}, Валюта: {newAccount.Currency} , ID операции {newAccount.Id},Комментарий:{newAccount.Comment}");
-
-            }
         }
 
-        private void ShowAccountsButton_Click(object sender, RoutedEventArgs e)
-        {
-            if(_accountingSystem._transactions.Count == null && _accountingSystem._accounts.Count == null && _accountingSystem._categories.Count== null) 
-            {
-                ShowAccountsList.Items.Add("Информация отсутствует");
-            }
-            else
-            {
-                ShowAccountsList.Items.Add("Транзакции:");
-                foreach(var transaction in _accountingSystem._transactions)
-                {
-                    ShowAccountsList.Items.Add($"Тип транзакции:{transaction.Key}, модель транзакции:{transaction.Value}");
-                }
-                ShowAccountsList.Items.Add("Счета:");
-                foreach(var account in _accountingSystem._accounts)
-                {
-                    ShowAccountsList.Items.Add($"Тип учетной записи:{account.Key},модель учетной записи:{account.Value}");
-                }
-                ShowAccountsList.Items.Add("Категории");
-                foreach(var category in _accountingSystem._categories)
-                {
-                    ShowAccountsList.Items.Add($"Катерогии по ID: {category.Key}, модель категории: {category.Value}");
-                }
-            }
-        }
         private void AddMoney_Click(object sender, RoutedEventArgs e)
         {
             AccountModel account = new AccountModel();
-            account.Balance = decimal.Parse(NumberText.Text);
+            account.AccountNumber = NumberText.Text;
             int summ = Int32.Parse(SummText.Text);
             _accountingSystem.AddMomey(account, summ);
-            ListBoxOne.Items.Add(account.Balance);
+            ListBoxOne.Items.Add($" Ваш баланс: {account.Balance}");
         }
 
         private void WithdrawMoney_Click(object sender, RoutedEventArgs e)
         {
             AccountModel model = new AccountModel();
-            model.Balance = decimal.Parse(NumberText.Text);
+            model.AccountNumber = NumberText.Text;
             int summ =Int32.Parse(SummText.Text);
             _accountingSystem.WithdrawMoney(model, summ);
-            ListBoxOne.Items.Add(model.Balance);
+            ListBoxOne.Items.Add($" Ваш баланс: {model.Balance}");
+        }
+
+        private void AddCategory_Click(object sender, RoutedEventArgs e)
+        {
+            CategoryModel model = new CategoryModel();
+            model.Name = NameCategoryText.Text;
+            _accountingSystem.AddCategory(model);
+            List.Items.Add($"Добавлена категория :{model.Name}");
+        }
+
+        private void DeleteCategory_Click(object sender, RoutedEventArgs e)
+        {
+            CategoryModel category = new CategoryModel();
+            category.Name = NameCategoryText.Text;
+            int Id = Int32.Parse(CategoryIdText.Text);
+            _accountingSystem.DeleteTransactionById(Id);
+            List.Items.Add($"Удалена категория с ID номером :{category.Id}");
+        }
+
+        private void IDCategoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            CategoryModel model = new CategoryModel();
+            model.Name = NameCategoryText.Text;
+            int Id =Int32.Parse(CategoryIdText.Text);
+            _accountingSystem.GetCategoryModelById(Id);
+            List.Items.Add($"Все категории с ID номером :{model.Id}");
+        }
+
+        private void CategoryList_Click(object sender, RoutedEventArgs e)
+        {
+            CategoryModel model = new CategoryModel();
+            _accountingSystem.GetAllCategoryModels();
+            List.Items.Add(_accountingSystem.GetAllCategoryModels);
         }
     }
 }
